00bcc9e416a0d3bb15f014636cfb51e0bee5adc7
Guillermo <guillermotr93@gmail.com>
2024-09-04 12:41:58 +0900

    displaying user profile

---
+++ amplify/auth/post-confirmation/resource.ts
@@ -0,0 +1,8 @@
+import { defineFunction } from '@aws-amplify/backend';
+/**
+ * Define and configure your post confimation resource
+ */
+
+export const postConfirmation = defineFunction({
+  name: 'post-confirmation',
+});
